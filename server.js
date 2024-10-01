c7f5d3aaf5f9c88d708c00574587365ccca44aac
Matheus Duarte <145275539+Duartois@users.noreply.github.com>
2024-09-30 21:06:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middlewares
-app.use(express.static("public_html"));
 app.use(express.json());
 
 // Configuração da AWS
@@ -533,6 +532,8 @@ app.get('/success', (req, res) => {
   console.log("Página de sucesso acessada com session_id:", req.query.session_id);
   res.sendFile("success.html", { root: "../front-end/public/pages" });
 });
+// Configuração para servir arquivos estáticos depois das rotas
+app.use(express.static("public_html"));
 // Rota 404
 app.get('/404', (req, res) => {
   res.sendFile("404.html", { root: "public_html/pages" });
