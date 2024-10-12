fc0f09e96a28d0b3d79f7ceac7f5212ddeb94f0a
Matheus Duarte <145275539+Duartois@users.noreply.github.com>
2024-10-12 12:16:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -337,11 +337,6 @@ app.post('/add-product', (req, res) => {
 });
 
 const generateTagVariants = (tag) => {
-    if (!tag || !tag.trim()) {
-        // Se a tag estiver vazia ou contiver apenas espaços em branco, retorna um array vazio
-        return [];
-    }
-
     const lowercaseTag = tag.toLowerCase();
     const uppercaseTag = tag.toUpperCase();
     const capitalizedTag = tag.charAt(0).toUpperCase() + tag.slice(1).toLowerCase();
