03d9c5cfae2031417fac6cbcce0db0a54c2b7dca
Matheus Duarte <145275539+Duartois@users.noreply.github.com>
2025-09-04 23:46:26 -0300

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -2,7 +2,7 @@ import express from "express";
 import Stripe from "stripe";
 
 const router = express.Router();
-const stripe = new Stripe(process.env.STRIPE_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 router.post("/stripe-checkout", async (req, res) => {
   try {
